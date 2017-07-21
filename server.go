28a02d1010ba865f9f55cfad8ff56ceb377ef15a
AdrienITTS <adrien.deprez.fr@gmail.com>
2017-07-21 22:52:11 +0200

    Added Errors Handling and some proof of Go format dirtylog

--- server.go
+++ server.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 // Person is a person
 type Person struct {
-	Name    string
-	Surname string
-	Age     int
+	Name                               string
+	Surname                            string
+	Age                                int
+	AVeryVeryLongData                  string
+	AVeryVeryLongDataAVeryVeryLongData string
 }
 
 func r(w http.ResponseWriter, r *http.Request) {
+	defer fmt.Println("Réponse envoyée !")
 	p := Person{Name: "Deprez", Surname: "Adrien", Age: 23}
-	payload, _ := json.Marshal(p)
+	payload, err := json.Marshal(p)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 	w.Header().Add("Content-Type", "application/json")
 	w.Write(payload)
 }
